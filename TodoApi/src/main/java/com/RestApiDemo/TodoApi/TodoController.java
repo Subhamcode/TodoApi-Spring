8cf1973f08f95195d9794364114c6bf7bbd8fb8d
Subham Agarwal <agarwalsubham79@gmail.com>
2024-04-17 16:05:01 +0530

    added post mapping request

--- TodoApi/src/main/java/com/RestApiDemo/TodoApi/TodoController.java
+++ TodoApi/src/main/java/com/RestApiDemo/TodoApi/TodoController.java
@@ -1,5 +1,8 @@
 package com.RestApiDemo.TodoApi;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -8,12 +11,23 @@ import java.util.List;
 @RestController
 public class TodoController {
 
-    private static List<Todo> todos;
+    private static List<Todo> todolist;
 
 
     public TodoController(){
-        todos=new ArrayList<>();
-        todos.add(new Todo(1,"Todo 1",1,false));
-        todos.add(new Todo(2,"Todo 2",2,true));
+        todolist=new ArrayList<>();
+        todolist.add(new Todo(1,"Todo 1",1,false));
+        todolist.add(new Todo(2,"Todo 2",2,true));
+    }
+
+    @GetMapping("/todos")
+    public List<Todo> getTodos(){
+        return todolist;
+    }
+
+    @PostMapping("/createTodo")
+    public Todo createTodo(@RequestBody Todo newTodo){
+            todolist.add(newTodo);
+            return newTodo;
     }
 }
